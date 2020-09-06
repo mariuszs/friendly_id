136c2d88cd723f9913b02462908332b596cd0232
Mariusz Smykula <mariuszs@gmail.com>
2020-09-06 15:34:24 +0200

    documentation and fix decode arg

--- src/friendly_id.rs
+++ src/friendly_id.rs
@@ -3,18 +3,47 @@ use uuid::Uuid;
 use crate::base62;
 use crate::error::DecodeError;
 
+///
+/// Create FriendlyID id
+/// # Examples
+///
+/// ```
+/// let result = friendly_id::create();
+///
+/// "5wbwf6yUxVBcr48AMbz9cb";
+/// ```
+///
 pub fn create() -> String {
     let uuid = Uuid::new_v4();
     return encode(&uuid);
 }
 
+///
+/// Encode UUID to FriendlyID id
+///
+/// ```
+/// let uuid = uuid::Uuid::parse_str("c3587ec5-0976-497f-8374-61e0c2ea3da5");
+/// friendly_id::encode(&uuid.unwrap());
+///
+/// "5wbwf6yUxVBcr48AMbz9cb";
+/// ```
+///
 pub fn encode(uuid: &Uuid) -> String {
     let data = uuid.as_u128();
     let base62 = base62::encode(data);
     return base62.to_string();
 }
 
-pub fn decode(id: String) -> Result<Uuid, DecodeError> {
+///
+/// Decode FriendlyID id to UUID
+///
+/// ```
+/// friendly_id::decode("5wbwf6yUxVBcr48AMbz9cb");
+///
+/// "c3587ec5-0976-497f-8374-61e0c2ea3da5";
+/// ```
+///
+pub fn decode(id: &str) -> Result<Uuid, DecodeError> {
     match base62::decode(id.as_str()) {
         Ok(base62) => return Ok(Uuid::from_u128(base62)),
         Err(e) => return Err(e)
@@ -29,13 +58,13 @@ mod tests {
 
     #[test]
     fn test_decode() {
-        assert_eq!(friendly_id::decode("5wbwf6yUxVBcr48AMbz9cb".to_string()).unwrap(),
+        assert_eq!(friendly_id::decode("5wbwf6yUxVBcr48AMbz9cb").unwrap(),
                    Uuid::parse_str("c3587ec5-0976-497f-8374-61e0c2ea3da5").unwrap());
     }
 
     #[test]
     fn test_decode_invalid_value() {
-        let result = friendly_id::decode("5+".to_string());
+        let result = friendly_id::decode("5+");
         assert!(result.is_err());
         let error = result.unwrap_err();
         println!("{}", error.to_string());
@@ -48,7 +77,7 @@ mod tests {
 
     #[test]
     fn test_encode() {
-        assert_eq!(friendly_id::encode(&Uuid::parse_str("c3587ec5-0976-497f-8374-61e0c2ea3da5").unwrap()), "5wbwf6yUxVBcr48AMbz9cb");
+        let uuid = Uuid::parse_str("c3587ec5-0976-497f-8374-61e0c2ea3da5");
+        assert_eq!(friendly_id::encode(&uuid.unwrap()), "5wbwf6yUxVBcr48AMbz9cb");
     }
-
 }
